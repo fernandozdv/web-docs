c796bb2b675530f30e8a08b07962763fc736ed52
fernandozdv <fernandozdv@gmail.com>
2017-01-12 22:21:21 -0500

    final-clase3

--- APIs/twitter.php
+++ APIs/twitter.php
@@ -1,5 +1,5 @@
 <?php 
-require "autoload.php";
+require "twitteroauth/autoload.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
   $ck="TA51tEkHm6eTPoaypcjQKtuZy";
   $cs="6we6tiPIzf6PujbZlc3B1hU1n5IFEZvwGYDMKztYnMaOaBK2rE";
--- php-d/projectPHP/core/core.php
+++ php-d/projectPHP/core/core.php
@@ -4,6 +4,6 @@
 define('HTML_DIR','html/');
 define('APP_TITLE','ForoZZZ');
 define('APP_DATE',date('Y',time()));
-
+require('vendor/autoload.php');
 
  ?>
